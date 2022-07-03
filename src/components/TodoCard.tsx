4f5724b90cbc61be95f41b8fa1dd67b5961eea9f
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-03 16:43:20 -0600

    Implement handleDone functionality

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -3,6 +3,7 @@ import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
 import '../styles/TodoCard.scss'
+import TodoList from './TodoList'
 
 type Props = {
   todo: Todo;
@@ -10,16 +11,32 @@ type Props = {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoCard = ({todo, todos, setTodos}: Props) => {
+const TodoCard = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) => (
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      ))
+    )
+  }
+
   return (
     <form className='todos__card'>
-      <span className="todos__card--text">{todo.todo}</span>
+      {
+        todo.isDone ? (
+          <s className="todos__card--text">{todo.todo}</s>
+        ) : (
+          <span className="todos__card--text">{todo.todo}</span>
+        )
+      }
       <div>
         <span className="icon"><AiFillEdit /></span>
         <span className="icon"><AiFillDelete /></span>
-        <span className="icon"><MdDone /></span>
+        <span className="icon">
+          <MdDone onClick={() => handleDone(todo.id)} />
+        </span>
       </div>
-    </form>
+    </form >
   )
 }
 
