539e75bf6cd7433c3e1e5d1373359d1fbe0bdadf
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-02 23:39:45 -0600

    Add useState & pass props to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './styles/App.scss'
 import NameList from './components/NameList'
 import InputField from './components/InputField'
+import { useState } from 'react'
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("")
+  console.log(todo);
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
       <NameList />
     </div>
   )
