8cadc6c8c3393a93f0426e4b1900bb15d31726e3
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-03 12:24:31 -0600

    Add useRef to remove focus on form submit

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../styles/inputField.scss'
 
-interface Props{
+interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
   handleAdd: (e: React.FormEvent) => void;
 }
 
 // const InputField = ({todo, setTodo}: Props) => {
-const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className='input' onSubmit={(e) => handleAdd(e)}>
+    <form className='input' onSubmit={(e) => {
+      handleAdd(e);
+      inputRef.current?.blur()
+    }}>
       <input
+        ref={inputRef}
         type='input'
         placeholder="Enter a task"
         className='input__box'
